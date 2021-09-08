b2b3fb31aee6d0d1d5c478f5b4c84a505f1967eb
Lwam Hadera <lwamhadera@Lwams-MacBook-Pro.local>
2021-09-08 01:59:44 -0500

    w2d2Objects

---
+++ w2d2Objects/calculator.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const calculator = {
+    a: 1,
+    b: 1,
+    setValues: function(x,y){
+        this.a = x;
+        this.b = y;
+    },
+    
+    sum: function(){
+        return this.a + this.b;
+    },
+
+    mul: function(){
+        return this.a * this.b;
+    }
+}; 
+
+
+
+
+
+
+
+
+
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
+/* must be at end of file if are exporting an object so the export is after the definition */
+module.exports = {calculator }; //add all of your object names here that you need for the node mocha tests
