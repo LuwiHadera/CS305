7f534d9f1935effee1b14488de4d4b297e07c36f
Lwam Hadera <lwamhadera@Lwams-MacBook-Pro.local>
2021-09-09 23:33:25 -0500

    W2D3 and W2D4

---
+++ W2D3/app.js
@@ -0,0 +1,50 @@
+"use strict";
+/* eslint-disable */
+
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
+module.exports = { ucFirst, checkSpam, truncate, camelize }; //add all of your function names here that you need for the node mocha tests
+
+function ucFirst(str) {
+  if (!str) {
+    return str;
+  }
+
+  let splited = str[0].toUpperCase() + str.slice(1);
+
+  return splited;
+}
+
+function checkSpam(str) {
+  let lower = str.toLowerCase();
+  let splited = lower.split(" ");
+  if (splited.includes("viagra") || splited.includes("xxxxx")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function truncate(str, maxlength) {
+  return str.length > maxlength ? str.slice(0, maxlength - 1) + "…" : str;
+}
+
+/**
+ * 
+ * @param {Array} arr of numbers
+ * @returns {number} the total of the maximal subarray
+ 
+ */
+// function getMaxSubSum(arr) {}
+
+function camelize(str) {
+  let splited = str.split("-");
+  let mapped = splited.map(function (item, index) {
+    if (index === 0) {
+      return item;
+    } else {
+      return item[0].toUpperCase() + item.slice(1);
+    }
+  });
+  let joined = mapped.join("");
+  return joined;
+}
