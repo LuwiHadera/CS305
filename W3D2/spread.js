589232c685090562103cf2050d8a9d858e17ae19
Lwam Hadera <lwamhadera@Lwams-MacBook-Pro.local>
2021-09-15 16:55:21 -0500

    W3D2

--- W3D2/spread.js
+++ W3D2/spread.js
@@ -2,7 +2,7 @@
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser 
 */
-//module.exports = {copyArray, concat, findMin, combineObjs }; //add all of your function names here that you need for the node mocha tests
+module.exports = {copyArray, concat, findMin, combineObjs }; //add all of your function names here that you need for the node mocha tests
 
 let arr = [3, 6, 9, 20];
 let arr2 = [4, 10, 20];
@@ -10,32 +10,33 @@ let obj = { x: 1, y: { a: 1, b: 2 } };
 let obj1 = { x1: 2, y1: { a: 2, b: 3 } };
 
 function copyArray(arr){
-    let copyArry = [...arr];
-   
+    let newArr = [...arr];
+   return newArr
 }
-console.log(copyArry(arr));
+//console.log(copyArray(arr));
 
 function concat(arr1, arr2){
-    let concatArr = arr.concat(arr2);
+    let concatArr = arr1.concat(arr2);
     
-    let concatArr1 = [...arr, ...arr2]; 
-    
+    let concatArr1 = [...arr1, ...arr2]; 
+  return concatArr;  
 }
 
-console.log(concatArr());
-console.log(concatArr());
+
+// console.log(concatArr());
+// console.log(concatArr());
 
 function findMin(...numbers) {
-    let min = Math.min(...arr); 
-    
+    let min = Math.min(...numbers); 
+    return min;
 }
-console.log(min);
+// console.log(min);
 
 function combineObjs(obj1, obj2){
-    let combineObj = { ...obj, ...obj1 };
-   
+    let combineObj = { ...obj1, ...obj2 };
+   return combineObj;
 }
-console.log(combineObj());
+// console.log(combineObj());
 
 
 
